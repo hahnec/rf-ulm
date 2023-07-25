4be191a1eb88d52a92916ebe66769271f1a642ca
Hahne Christopher (ch21z139) <ch21z139@submit01.ubelix.unibe.ch>
2023-07-25 23:14:51 +0200

    fix(sh): update venv and bash scripts

--- infer_3090.sh
+++ infer_3090.sh
@@ -6,27 +6,24 @@
 #SBATCH --mail-user=christopher.hahne@unibe.ch
 #SBATCH --mail-type=none
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=98G
+#SBATCH --mem-per-cpu=80G
 #SBATCH --qos=job_gpu_sznitman
 #SBATCH --account=ws_00000
 #SBATCH --partition=gpu-invest
 #SBATCH --gres=gpu:rtx3090:1
 
-module load Python/3.8.6-GCCcore-10.2.0
-#module load CUDA/11.3.0-GCC-10.2.0
-#module load cuDNN/8.2.0.53-CUDA-11.3.0
-#module load Workspace
-
-python -m venv venv
+module load Python/3.9.5-GCCcore-10.3.0
+module load CUDA/11.8.0
 
 source ~/20_sr-ulm/venv/bin/activate
 
-python -m pip install -r requirements.txt
+#python3 -m pip install -r requirements.txt
+#python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
-git clone --recurse-submodules git@github.com:hahnec/simple_tracker simple_tracker_repo
-python3 -m pip install -r simple_tracker_repo/requirements.txt
-ln -sf ./simple_tracker_repo/simple_tracker simple_tracker
+#git clone --recurse-submodules git@github.com:hahnec/simple_tracker simple_tracker_repo
+#python3 -m pip install -r simple_tracker_repo/requirements.txt
+#ln -sf ./simple_tracker_repo/simple_tracker simple_tracker
 
-python -c "import torch; print(torch.cuda.is_available())"
+python3 -c "import torch; print(torch.cuda.is_available())"
 
-python ./predict.py
+python3 ./predict.py
