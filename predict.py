d1fd1cc44177731f191039f01d5f51f4d87f5e18
hahnec <christopher.hahne@unibe.ch>
2023-07-27 16:47:41 +0200

    fix(str): conver to string before calling lower()

--- predict.py
+++ predict.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     origin = np.array([cfg.origin_x, cfg.origin_z])
     wv_idcs = [1] if cfg.input_type == 'iq' else list(range(3))
     name_ext = '_' + str(int(cfg.upscale_factor)) + '_' + str(int(cfg.rescale_factor))
-    fnames = [fname for fname in Path('.').iterdir() if fname.lower().__contains__('t_mats' + name_ext)]
+    fnames = [fname for fname in Path('.').iterdir() if str(fname).lower().__contains__('t_mats' + name_ext)]
     t_mats = np.load(fnames[0]) if cfg.input_type == 'rf' else np.zeros((3,3,3))
     # flip matrices to avoid coordinate flipping during inference
     t_mats[:, :2] = t_mats[:, :2][:, ::-1]
