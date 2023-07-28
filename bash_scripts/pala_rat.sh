b4fcd828403e55226ca3e84210d52aa829d3ed25
Hahne Christopher (ch21z139) <ch21z139@submit01.ubelix.unibe.ch>
2023-07-28 20:52:12 +0200

    fix(rat): one job after another to avoid crashes

--- bash_scripts/pala_rat.sh
+++ bash_scripts/pala_rat.sh
@@ -6,12 +6,12 @@
 #SBATCH --mail-user=christopher.hahne@unibe.ch
 #SBATCH --mail-type=none
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=80G
+#SBATCH --mem-per-cpu=92G
 #SBATCH --qos=job_gpu_sznitman
 #SBATCH --account=ws_00000
 #SBATCH --partition=gpu-invest
 #SBATCH --gres=gpu:rtx3090:1
-#SBATCH --array=1-3%3
+#SBATCH --array=1-3%1
 
 module load Python/3.9.5-GCCcore-10.3.0
 module load CUDA/11.8.0
@@ -25,5 +25,6 @@ param_store=~/20_sr-ulm/bash_scripts/array_pala_params.txt
 model=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
 model_file=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $2}')
 threshold=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $3}')
+type=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $4}')
 
-python3 ./predict.py model=${model} model_file=${model_file} th=${threshold} batch_size=1 pow_law_opt=True data_dir=/storage/workspaces/artorg_aimi/ws_00000/chris/Rat18_2D_InVivoBrain/ logging=pala_rat 
+python3 ./predict.py model=${model} model_file=${model_file} nms_threshold=${threshold} batch_size=1 pow_law_opt=True data_dir=/storage/workspaces/artorg_aimi/ws_00000/chris/Rat18_2D_InVivoBrain/ logging=pala_rat
