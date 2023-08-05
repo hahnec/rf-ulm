2607a22f9ffc4083ede9d9db2b73861ab8020b4b
hahnec <christopher.hahne@unibe.ch>
2023-08-05 21:53:20 +0200

    fix(wv): only cluster points if available

--- evaluate.py
+++ evaluate.py
@@ -47,9 +47,10 @@ def evaluate(model, dataloader, amp, cfg, t_mats):
 
             # point fusion from compounded waves
             if len(cfg.wv_idcs) > 1:
-                pts = np.hstack([el for el in [wv_es_points[1][0], wv_es_points[0][0], wv_es_points[2][0]] if el.size > 0])
-                # fuse points using DBSCAN when eps > 0 and 
-                es_points = [cluster_points(pts[:2].T, cluster_obj=cluster_obj).T] if pts.size > 0 and cfg.eps > 0 else [pts]
+                wv_list = [el for el in [wv_es_points[1][0], wv_es_points[0][0], wv_es_points[2][0]] if el.size > 0]
+                if len(wv_list) > 0:
+                    # fuse points using DBSCAN when eps > 0 and 
+                    es_points = [cluster_points(pts[:2].T, cluster_obj=cluster_obj).T] if pts.size > 0 and cfg.eps > 0 else [pts]
             else:
                 es_points = wv_es_points[0]
 
