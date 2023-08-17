ff880e0fe519c278c0a5f2c2068b274658c25007
hahnec <christopher.hahne@unibe.ch>
2023-08-18 01:43:13 +0200

    fix(pad): ensure odd integer padding in non-maximum suppression

--- utils/nms_funs.py
+++ utils/nms_funs.py
@@ -3,8 +3,13 @@ import torch
 
 def non_max_supp_torch(frame, size=3, norm_opt=False):
 
+    # ensure odd integer padding
+    size = size//2*2 - 1
+
+    # max-pooling
     mask = torch.nn.functional.max_pool2d(frame.clone(), kernel_size=size, stride=1, padding=size//2)
 
+    # suppression of non-maxima
     mask[mask != frame] = 0
 
     return mask
