56e4e39871a859956bcf3657937ddcb40c16e7c9
hahnec <christopher.hahne@unibe.ch>
2023-09-03 17:36:15 +0200

    refactor(clone): remove clone for performance

--- utils/nms_funs.py
+++ utils/nms_funs.py
@@ -7,7 +7,7 @@ def non_max_supp_torch(frame, size=3, norm_opt=False):
     size = size//2*2 - 1
 
     # max-pooling
-    mask = torch.nn.functional.max_pool2d(frame.clone(), kernel_size=size, stride=1, padding=size//2)
+    mask = torch.nn.functional.max_pool2d(frame, kernel_size=size, stride=1, padding=size//2)
 
     # suppression of non-maxima
     mask[mask != frame] = 0
