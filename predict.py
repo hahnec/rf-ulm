fix(rf): rectify coordinate scaling part 2

predict.py
@@ -55,7 +55,7 @@ def render_ulm_frame(all_pts, imgs, img_size, cfg, fps, scale=None):
     # consider 
     if cfg.model == 'sgspcn' and cfg.skip_bmode:
         s = 128/143
-        all_pts = [np.array([p[:, 0]/s, p[:, 1]]).T for p in all_pts if p.size > 0]
+        all_pts = [np.array([p[:, 0]*s, p[:, 1]]).T for p in all_pts if p.size > 0]
         old_size = img_size
         img_size[1] = 128
 