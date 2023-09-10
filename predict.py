8001d757b7e1f50c02cb6dec8f70a9b002586eda
hahnec <christopher.hahne@unibe.ch>
2023-09-10 11:59:28 +0200

    feat(rf): refine RF-based ULM interpolation

--- predict.py
+++ predict.py
@@ -56,9 +56,9 @@ def render_ulm_frame(all_pts, imgs, img_size, cfg, fps, scale=None):
     ref_size = img_size.copy()
 
     # consider 
-    if cfg.model == 'sgspcn' and cfg.skip_bmode:
+    if cfg.model == 'sgspcn' and cfg.skip_bmode and not cfg.dither and cfg.upscale_factor <= 10:
         s = 128/ref_size[1]
-        d = 128-ref_size[1] / 2
+        d = 128-ref_size[1] / 2 
         all_pts = [np.array([p[:, 0]*s, p[:, 1]]).T for p in all_pts if p.size > 0]
         old_size = ref_size.copy()
         ref_size[1] = 128
@@ -79,7 +79,12 @@ def render_ulm_frame(all_pts, imgs, img_size, cfg, fps, scale=None):
         sres_ulm_img, _ = tracks2img(all_pts, img_size=ref_size, scale=scale, mode=cfg.track, fps=fps)
 
     if ref_size[1] == 128:
-        sres_ulm_img = resize(sres_ulm_img, (sres_ulm_img.shape[0], old_size[1]*cfg.upscale_factor), anti_aliasing=False)
+        # horizontal interpolation only
+        from scipy import interpolate
+        x = np.arange(sres_ulm_img.shape[1])
+        interp_func = interpolate.interp1d(x, sres_ulm_img, kind='linear', axis=1, fill_value='extrapolate')
+        new_x = np.linspace(0, sres_ulm_img.shape[1] - 1, old_size[1]*cfg.upscale_factor)
+        sres_ulm_img = interp_func(new_x)
 
     return sres_ulm_img
 
