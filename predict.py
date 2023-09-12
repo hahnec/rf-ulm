3fdd22f1cd384c2c347750dae0068f3a5be0fb97
hahnec <christopher.hahne@unibe.ch>
2023-09-12 19:26:01 +0200

    fix(trunc): truncate lower bound correctly

--- predict.py
+++ predict.py
@@ -42,7 +42,7 @@ from utils.dithering import dithering
 
 normalize = lambda x: (x-x.min())/(x.max()-x.min()) if x.max()-x.min() > 0 else x-x.min()
 img_color_map = lambda img, cmap: plt.get_cmap(cmap)(img)[..., :3]
-truncate_outliers = lambda x,q=.05: np.piecewise(x, [x < np.quantile(x, q), x > np.quantile(x, 1-q)], [0, np.quantile(x, 1-q)])
+truncate_outliers = lambda x,q=.05: np.piecewise(x, [x < np.quantile(x, q), x > np.quantile(x, 1-q)], [np.quantile(x, q), np.quantile(x, 1-q)])
 ulm_align = lambda img, gamma, cmap: img_color_map(img=srgb_conv(normalize(truncate_outliers(img)**gamma)), cmap=cmap)
 
 
