d82e3849df31ccc5361fd0dfb51de14eadd6c7d4
hahnec <christopher.hahne@unibe.ch>
2023-10-02 22:02:49 +0200

    feat(env): add installation routine

--- install_env.sh
+++ install_env.sh
@@ -1,2 +1,8 @@
-# link to simple tracker
+# git submodules
+git submodule update --init --recursive
 ln -sf ./simple_tracker_repo/simple_tracker ./simple_tracker
+
+# virtual environment
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
