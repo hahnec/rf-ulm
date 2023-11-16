894554aad5398144a9821974abe50d44052f1bf2
hahnec <christopher.hahne@unibe.ch>
2023-11-16 16:12:52 +0100

    refactor(render): place render function in separate utils file

--- predict.py
+++ predict.py
@@ -41,6 +41,7 @@ from utils.utils import plot_img_and_mask
 from utils.transform import ArgsToTensor, NormalizeImage, NormalizeVol
 from utils.point_fusion import cluster_points
 from utils.dithering import dithering
+from utils.render_ulm import render_ulm_frame
 
 
 normalize = lambda x: (x-x.min())/(x.max()-x.min()) if x.max()-x.min() > 0 else x-x.min()
@@ -49,54 +50,6 @@ truncate_outliers = lambda x, q=1e-4: np.where(x < np.quantile(x, q), np.quantil
 ulm_scale = lambda img, gamma: srgb_conv(normalize(truncate_outliers(img)**gamma))
 ulm_align = lambda img, gamma, cmap: img_color_map(img=ulm_scale(img, gamma), cmap=cmap)
 velo_cmap = LinearSegmentedColormap.from_list('custom_colormap', [(2/3, 1, 1),(0, 1/3, 1), (0, 0, 0), (1, 1/3, 0), (1, 1, 2/3)], N=2**8)
-
-
-def render_ulm_frame(all_pts, imgs, img_size, cfg, fps, scale=None, interpol_method=0):
-    
-    scale = 10 if scale is None else scale
-
-    # for point dimension consistency
-    all_pts = [p[:, :2] for p in all_pts if p.size > 0]
-    
-    # keep original variable
-    ref_size = img_size.copy()
-
-    # consider RF-based point density
-    if cfg.model == 'sgspcn' and cfg.skip_bmode and not cfg.dither and interpol_method > 0:
-        s = 128/ref_size[1]
-        t = 256/ref_size[0] if interpol_method == 2 else 1
-        all_pts = [np.array([p[:, 0]*s, p[:, 1]*t]).T for p in all_pts if p.size > 0]
-        old_size = ref_size.copy()
-        ref_size[1] = 128
-        if interpol_method == 2: ref_size[0] = 256
-
-    if cfg.dither:
-        # dithering
-        img_shape = np.array(imgs[0].shape[-2:])[::-1] if cfg.input_type == 'rf' else ref_size
-        y_factor, x_factor = img_shape / ref_size
-        all_pts = dithering(all_pts, cfg.upscale_factor, cfg.upscale_factor, x_factor, y_factor)
-
-    if cfg.upscale_factor < scale and not cfg.dither:
-        sres_ulm_img, velo_ulm_img = tracks2img(all_pts, img_size=ref_size, scale=cfg.upscale_factor, mode=cfg.track, fps=fps)
-        if cfg.upscale_factor != 1:
-            sres_ulm_img = cv2.resize(sres_ulm_img, scale*ref_size[::-1], interpolation=cv2.INTER_CUBIC)
-            sres_ulm_img[sres_ulm_img<0] = 0
-    else:
-        sres_ulm_img, velo_ulm_img = tracks2img(all_pts, img_size=ref_size, scale=scale, mode=cfg.track, fps=fps)
-
-    if ref_size[1] == 128:
-        if ref_size[0] == 256:
-            # 2-D interpolation
-            sres_ulm_img = cv2.resize(sres_ulm_img, scale*old_size[::-1], interpolation=cv2.INTER_CUBIC)
-            sres_ulm_img[sres_ulm_img<0] = 0
-        else:
-            # horizontal interpolation only
-            x = np.arange(sres_ulm_img.shape[1])
-            interp_func = interpolate.interp1d(x, sres_ulm_img, kind='linear', axis=1, fill_value='extrapolate')
-            new_x = np.linspace(0, sres_ulm_img.shape[1] - 1, old_size[1]*scale)
-            sres_ulm_img = interp_func(new_x)
-
-    return sres_ulm_img, velo_ulm_img
 
 
 if __name__ == '__main__':
---
+++ utils/render_ulm.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+
+
+def render_ulm_frame(all_pts, imgs, img_size, cfg, fps, scale=None, interpol_method=0):
+    
+    scale = 10 if scale is None else scale
+
+    # for point dimension consistency
+    all_pts = [p[:, :2] for p in all_pts if p.size > 0]
+    
+    # keep original variable
+    ref_size = img_size.copy()
+
+    # consider RF-based point density
+    if cfg.model == 'sgspcn' and cfg.skip_bmode and not cfg.dither and interpol_method > 0:
+        s = 128/ref_size[1]
+        t = 256/ref_size[0] if interpol_method == 2 else 1
+        all_pts = [np.array([p[:, 0]*s, p[:, 1]*t]).T for p in all_pts if p.size > 0]
+        old_size = ref_size.copy()
+        ref_size[1] = 128
+        if interpol_method == 2: ref_size[0] = 256
+
+    if cfg.dither:
+        # dithering
+        img_shape = np.array(imgs[0].shape[-2:])[::-1] if cfg.input_type == 'rf' else ref_size
+        y_factor, x_factor = img_shape / ref_size
+        all_pts = dithering(all_pts, cfg.upscale_factor, cfg.upscale_factor, x_factor, y_factor)
+
+    if cfg.upscale_factor < scale and not cfg.dither:
+        sres_ulm_img, velo_ulm_img = tracks2img(all_pts, img_size=ref_size, scale=cfg.upscale_factor, mode=cfg.track, fps=fps)
+        if cfg.upscale_factor != 1:
+            sres_ulm_img = cv2.resize(sres_ulm_img, scale*ref_size[::-1], interpolation=cv2.INTER_CUBIC)
+            sres_ulm_img[sres_ulm_img<0] = 0
+    else:
+        sres_ulm_img, velo_ulm_img = tracks2img(all_pts, img_size=ref_size, scale=scale, mode=cfg.track, fps=fps)
+
+    if ref_size[1] == 128:
+        if ref_size[0] == 256:
+            # 2-D interpolation
+            sres_ulm_img = cv2.resize(sres_ulm_img, scale*old_size[::-1], interpolation=cv2.INTER_CUBIC)
+            sres_ulm_img[sres_ulm_img<0] = 0
+        else:
+            # horizontal interpolation only
+            x = np.arange(sres_ulm_img.shape[1])
+            interp_func = interpolate.interp1d(x, sres_ulm_img, kind='linear', axis=1, fill_value='extrapolate')
+            new_x = np.linspace(0, sres_ulm_img.shape[1] - 1, old_size[1]*scale)
+            sres_ulm_img = interp_func(new_x)
+
+    return sres_ulm_img, velo_ulm_img
+
