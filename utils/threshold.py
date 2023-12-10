f4668c544cf3ae108fdb480f88c127ade9a02ec0
hahnec <christopher.hahne@unibe.ch>
2023-12-10 16:32:27 +0100

    refactor(var): adapt variable name and analysis tooling

--- utils/threshold.py
+++ utils/threshold.py
@@ -4,13 +4,31 @@ from sklearn.metrics import roc_curve
 from sklearn.metrics import precision_recall_curve
 
 
-def estimate_threshold(true_masks, masks_pred):
+def estimate_threshold(true_masks, pred_masks):
 
     # calculate the g-mean for each threshold
-    fpr, tpr, thresholds = roc_curve(true_masks.float().cpu().numpy().flatten(), masks_pred.float().cpu().numpy().flatten())
-    #precision, recall, thresholds = precision_recall_curve(mask_true.float().numpy().flatten(), masks_pred.float().numpy().flatten())
+    fpr, tpr, thresholds = roc_curve(true_masks.float().cpu().numpy().flatten(), pred_masks.float().cpu().numpy().flatten())
+    #precision, recall, thresholds = precision_recall_curve(mask_true.float().numpy().flatten(), pred_masks.float().numpy().flatten())
     gmeans = (tpr * (1-fpr))**.5
     th_idx = np.argmax(gmeans)
     threshold = thresholds[th_idx]
+
+    if False:
+        from sklearn.metrics import RocCurveDisplay
+        RocCurveDisplay.from_predictions(
+            true_masks.float().cpu().numpy().flatten(),
+            pred_masks.float().cpu().numpy().flatten(),
+            name="micro-average OvR",
+            color="darkorange")
+        from sklearn.metrics import auc
+        a = auc(fpr, tpr)
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(0,1.1,0.1),np.arange(0,1.1,0.1),linestyle='-.',color='k')
+        plt.axis("square")
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
+        plt.title("Micro-averaged One-vs-Rest\nReceiver Operating Characteristic")
+        plt.legend({'AUC for classifier: '+str(a)})
+        plt.show()
 
     return threshold
